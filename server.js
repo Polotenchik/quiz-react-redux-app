b34688c4e16fcc65e0166581dff3bed982b6217c
Polotenchik Koala <Polotenchik.koala@gmail.com>
2019-01-20 22:42:06 +0300

    feat: create mongoose schemas and add apollo-express middleware

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const db = require('./config/keys').mongoURI;
+const bodyParser = require('body-parser');
+//const db = require('./config/keys').mongoURI;
+
+const Recipe = require('./models/Recipe');
+const User = require('./models/User');
+
+const { graphiqlExpress, graphqlExpress } = require('apollo-server-express');
+const { makeExecutableSchema } = require('graphql-tools');
+
+
+const { typeDefs } = require('./schema');
+const { resolvers } = require('./resolvers');
+
+
+const schema = makeExecutableSchema({
+    typeDefs: typeDefs,
+    resolvers: resolvers
+});
 
 // mongoose
 //     .connect(db, { useNewUrlParser: true })
@@ -8,6 +25,18 @@ const db = require('./config/keys').mongoURI;
 //     .catch(err => console.log(err));
 
 const app = express();
+
+app.use('/graphiql', graphiqlExpress({
+    endpointURL: '/graphql'
+}));
+
+app.use('/graphql', graphqlExpress({
+    schema,
+    context: {
+        Recipe,
+        User
+    }
+}));
 
 const PORT = process.env.PORT || 8000;
 
