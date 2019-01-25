e7aa5a77b0f2897dbe9f7c7d5e2848a4cd1d7a11
Polotenchik Koala <Polotenchik.koala@gmail.com>
2019-01-25 23:22:57 +0300

    feat: add Query to App

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import { Query } from 'react-apollo';
+import { GET_ALL_RECIPES } from  '../queries';
+
+
 class App extends Component {
   render() {
     return (
