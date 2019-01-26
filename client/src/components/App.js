ab1583beba4aaac10bdd0881f9d9cd2b19db667a
Polotenchik Koala <Polotenchik.koala@gmail.com>
2019-01-26 21:24:43 +0300

    feat: add Query to App.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,15 +4,28 @@ import './App.css';
 import { Query } from 'react-apollo';
 import { GET_ALL_RECIPES } from  '../queries';
 
+const App = () => (
+  <div className="App">
+    <h1>Home</h1>
+    <Query query={GET_ALL_RECIPES}>
+      {
+        ({data, loading, error}) => {
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        Home
-      </div>
-    );
-  }
-}
+          if (loading) {
+            return <div>Loading</div>
+          }
+
+          if (error) {
+            return <div>Error</div>
+          }
+
+          return (
+            <p>Recipes</p>
+          )
+        }
+      }
+    </Query>
+  </div>
+)
 
 export default App;
